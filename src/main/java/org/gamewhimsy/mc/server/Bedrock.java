d235ccdb0fc58dcda7e6cb637dca54337fb7b550
Tulonsae <admin@tulonsae.org>
2012-09-15 14:30:01 -0700

    Setup initial doc files, pom, and empty main class.

---
+++ src/main/java/org/gamewhimsy/mc/server/Bedrock.java
@@ -0,0 +1,20 @@
+package org.gamewhimsy.mc.server;
+
+/**
+ * Main class.
+ *
+ * Copyright (c) 2012 Tulonsae.
+ */
+public class Bedrock {
+
+    private static final String copyright = "Copyright (c) 2012 Tulonsae.";
+    private static final String license = "MIT-style license.";
+
+    /**
+     * Main entry.
+     *
+     * @param args Arguments (unused).
+     */
+    public static void main(String[] args) {
+    }
+}
