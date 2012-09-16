bd42187017080726a09c00f84b990c1f6844ff79
Tulonsae <admin@tulonsae.org>
2012-09-15 18:18:02 -0700

    Added console, using jline 2.8.

--- src/main/java/org/gamewhimsy/mc/server/Bedrock.java
+++ src/main/java/org/gamewhimsy/mc/server/Bedrock.java
@@ -1,5 +1,10 @@
 package org.gamewhimsy.mc.server;
 
+import java.io.IOException;
+
+import jline.TerminalFactory;
+import jline.console.ConsoleReader;
+
 /**
  * Main class.
  *
@@ -10,11 +15,29 @@ public class Bedrock {
     private static final String copyright = "Copyright (c) 2012 Tulonsae.";
     private static final String license = "MIT-style license.";
 
+    private static final String prompt = "Bedrock> ";
+
     /**
      * Main entry.
      *
      * @param args Arguments (unused).
      */
     public static void main(String[] args) {
+        try {
+            ConsoleReader console = new ConsoleReader();
+            console.setPrompt(prompt);
+            String line = null;
+            while ((line = console.readLine()) != null) {
+                console.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                TerminalFactory.get().restore();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
